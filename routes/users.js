ac45e722b63ec54342d8f32312511638deae52d5
Okode100 <alfakings73@gmail.com>
2025-06-01 23:12:54 +0300

    updated the POST handle to handle the router for the emails

--- routes/users.js
+++ routes/users.js
@@ -53,8 +53,32 @@ router.post("/",(req,res)=>{
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((users) => users.email === email);
+
+  if (filtered_users.length > 0) {
+    let filtered_user = filtered_users[0];  // Fixed: Correctly access first user
+
+    let DOB = req.query.DOB;
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+
+    if (DOB) {
+      filtered_user.DOB = DOB;
+    }
+    if (firstName) {
+      filtered_user.firstName = firstName;
+    }
+    if (lastName) {
+      filtered_user.lastName = lastName;
+    }
+
+    users = users.filter((user) => user.email != email);
+    users.push(filtered_user);
+    res.send(`User with email ${email} updated`);
+  } else {
+    res.send("User not found");
+  }
 });
 
 
